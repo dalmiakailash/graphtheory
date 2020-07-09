000821b5f1ac5dab86797e15ea36b1f77f7de512
dalmiakailash <50864698+dalmiakailash@users.noreply.github.com>
2020-07-09 12:12:43 +0530

    Some more hints and compilation issues fix

--- src/RomeCity.java
+++ src/RomeCity.java
@@ -50,50 +50,22 @@ class RomeCity {
 			return -1;
 		}
 		return ab.values().iterator().next();
-	}
-
-	public int solution2(int[] A, int[] B) {
-		if (A.length < B.length) {
-			return -1;
-		}
-		final int[] C = new int[A.length];
-
-		final Map<Integer, Integer> ab = new HashMap<>();
-		final Map<Integer, Integer> ba = new HashMap<>();
-		for (int i = 0; i < A.length; i++) {
-			final int a = A[i];
-			final int b = B[i];
-			if (ab.isEmpty()) {
-				ab.put(a, b);
-				ba.put(b, a);
-			} else {
-				ab.put(a, ab.getOrDefault(b, b));
-				ba.put(b, a);
-				if (ba.containsKey(a)) {
-					ab.put(ba.get(a), b);
-				}
-			}
-		}
-		if (new HashSet<>(ab.values()).size() > 1) {
-			return -1;
-		}
-		return ab.values().iterator().next();
-	}
+	}	
 
 	public static void main(String[] args) {
-		System.out.println(new Solution().solution(new int[] {0, 1, 2, 4, 5}, new int[] {2, 3, 3, 3, 2}));
-		System.out.println(new Solution().solution(new int[] {1,2,3}, new int[] {0,0,0}));
-		System.out.println(new Solution().solution(new int[] {2,3,3,4}, new int[] {1,1,0,0}));
-		System.out.println(new Solution().solution(new int[] {0, 1, 2, 4, 5, 6}, new int[] {2, 3, 3, 3, 2, 2}));
-		System.out.println(new Solution().solution(new int[] {0, 1, 2, 4, 5, 6}, new int[] {2, 3, 3, 3, 2, 0}));
-		System.out.println(new Solution().solution(new int[] {0, 1, 2, 4, 5, 5}, new int[] {2, 3, 3, 3, 2, 6}));
-		System.out.println(new Solution().solution(new int[] {0, 1, 2, 4, 5, 6}, new int[] {2, 3, 3, 3, 2, 5}));
-		System.out.println(new Solution().solution(new int[] {0, 1, 3, 4, 5, 6}, new int[] {3, 2, 2, 2, 3, 5}));
-		System.out.println(new Solution().solution(new int[] {0, 1, 3, 4, 5, 7}, new int[] {3, 2, 2, 2, 3, 4}));
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 2, 4, 5}, new int[] {2, 3, 3, 3, 2})); //3
+		System.out.println(new RomeCity().solution(new int[] {1,2,3}, new int[] {0,0,0})); //0
+		System.out.println(new RomeCity().solution(new int[] {2,3,3,4}, new int[] {1,1,0,0})); //-1
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 2, 4, 5, 6}, new int[] {2, 3, 3, 3, 2, 2}));//3
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 2, 4, 5, 6}, new int[] {2, 3, 3, 3, 2, 0}));//3
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 2, 4, 5, 5}, new int[] {2, 3, 3, 3, 2, 6}));//-1
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 2, 4, 5, 6}, new int[] {2, 3, 3, 3, 2, 5}));//3
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 3, 4, 5, 6}, new int[] {3, 2, 2, 2, 3, 5}));//2
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 3, 4, 5, 7}, new int[] {3, 2, 2, 2, 3, 4}));//2
 
-		System.out.println(new Solution().solution(new int[] {0, 1, 3, 4, 5, 7, 8, 10, 13, 15, 17, 20}, new int[] {3, 2,
-				2, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-		//System.out.println(new Solution().solution(new int[] {0, 1, 3, 4, 5, 7}, new int[] {3, 2, 2, 2, 3, 4}));
-		//System.out.println(new Solution().solution(new int[] {0, 1, 3, 4, 5, 7}, new int[] {3, 2, 2, 2, 3, 4}));
+		System.out.println(new RomeCity().solution(new int[] {0, 1, 3, 4, 5, 7, 8, 10, 13, 15, 17, 20}, new int[] {3, 2,
+				2, 2, 3, 4, 5, 6, 7, 8, 9, 10})); //-1
+		//System.out.println(new RomeCity().solution(new int[] {0, 1, 3, 4, 5, 7}, new int[] {3, 2, 2, 2, 3, 4}));
+		//System.out.println(new RomeCity().solution(new int[] {0, 1, 3, 4, 5, 7}, new int[] {3, 2, 2, 2, 3, 4}));
 	}
 }
